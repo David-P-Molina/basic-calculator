fed40ccc96c96a46c2969d316b08f39495db2033
David Moina <davemolina777@gmail.com>
2021-12-14 14:31:48 -0600

    Create basic app divs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ function App() {
   return (
     <div className="App">
       <h1>Basic Calculator</h1>
+        <div className="calculator">
+            <div className="display">
+                <span>(0)</span> 0
+            </div>
+                <div className="operators">
+        
+                </div>
+                <div className="digits">
+
+                </div>
+        </div>
     </div>
   );
 }
